52efdc60a65cb3a9b695ae9e88788338c3793e7d
Abbo <pedronicolasabba@hotmail.com>
2017-11-08 15:23:51 -0300

    Commit Grafico

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ComentarioTest < ActiveSupport::TestCase
+class QuestionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
