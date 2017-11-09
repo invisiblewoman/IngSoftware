0a8921d79d675a1fd3747a41d04914490fec5444
Abbo <pedronicolasabba@hotmail.com>
2017-11-09 01:01:09 -0300

    Primer Commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,9 @@
 class QuestionsController < ApplicationController
 	def index
 	end
+	def destroy
+	    question = Question.find(params[:id])
+	    question.destroy
+	    redirect_to monstruos_path
+	end
 end
