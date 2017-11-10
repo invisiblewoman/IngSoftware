b5fb741f65e9e659f734406d424fa1b50d701cad
MartinG <96martingalan@gmail.com>
2017-11-09 22:35:26 -0300

    Anda el I/O user y el listado de preguntas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,59 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'comments/new'
+
+  get 'comments/edit'
+
+  get 'comments/create'
+
+  get 'comments/update'
+
+  get 'comments/destroy'
+
+  get 'tags/index'
+
+  get 'tags/show'
+
+  get 'tags/new'
+
+  get 'tags/edit'
+
+  get 'tags/create'
+
+  get 'tags/update'
+
+  get 'tags/destroy'
+
+  get 'answers/index'
+
+  get 'answers/show'
+
+  get 'answers/new'
+
+  get 'answers/edit'
+
+  get 'answers/create'
+
+  get 'answers/update'
+
+  get 'answers/destroy'
+
+ 
+
   get 'default/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'question#index'
+  
+  resources :questions
+  resources :answers
+  resources :comments
+  resources :tags
+  resources :users
+  root 'questions#index'
 end
+
+
---
+++ test/controllers/newquestion_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NewquestionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
