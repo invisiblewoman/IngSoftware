cb0619eecd92b85c226a63871875aaef26381e37
Abbo <pedronicolasabba@hotmail.com>
2017-11-10 08:28:59 -0300

    Hicimos que anden los botones

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171109011628) do
-
 
   create_table "answers", force: :cascade do |t|
     t.string "texto"
@@ -46,7 +44,6 @@ ActiveRecord::Schema.define(version: 20171109011628) do
 
   create_table "tags", force: :cascade do |t|
     t.string "nombre"
-<<<<<<< HEAD
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -67,6 +64,10 @@ ActiveRecord::Schema.define(version: 20171109011628) do
     t.string "descripcion"
     t.integer "votos"
     t.string "foto"
-
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
