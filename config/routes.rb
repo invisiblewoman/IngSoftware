ea4001ada359a973627679fd1ac0ea9c29688f60
Abbo <pedronicolasabba@hotmail.com>
2017-11-16 00:14:35 -0300

    Esto andaria

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
 
   resources :questions do
    resources :answers 
+   resources :comments
+  end
+
+  resources :answers do
+    resources :comments
     
   
   end
