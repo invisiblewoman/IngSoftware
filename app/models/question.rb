530a8a3ecdb882be1c814b0063e5b94c14bb5265
Mag <96martingalan@gmail.com>
2017-11-22 17:11:32 -0300

    Footer y Mesajes de errores

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,7 @@ class Question < ApplicationRecord
 	has_many :tags,through: :question_tags
 	has_many :answer, dependent: :destroy
 	has_many :question_comment,dependent:  :destroy
+	scope :search,->(parametro){
+		where('titulo LIKE ? OR cuerpo LIKE ?', "%#{parametro}%", "%#{parametro}%")
+	}
 end
