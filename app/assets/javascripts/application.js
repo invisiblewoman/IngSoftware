cc15edbd4a7283132a50ebec5a9cbef1dcbc0934
MartinG <96martingalan@gmail.com>
2017-11-23 21:40:56 -0300

    Text rich

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require tinymce-jquery
 //= require_tree .
+
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 	before_action :authenticate_user!,except:[:index,:show]
 	def index
-		@condicion = params[:condicion]
+		
 		if params[:buscar]
 			@questions = Question.search(params[:buscar])
 		else 
@@ -14,12 +14,14 @@ class QuestionsController < ApplicationController
 	def destroy
 	    question = Question.find(params[:id])
 	    question.destroy
-	    redirect_to monstruos_path
+	    redirect_to questions_path
 	end
 	def new
+		@condicion = params[:condicion]
 		@question = Question.new
 	end
 	def create
+		
 		@question = Question.new(
 		params.require(:question)
 		.permit(:cuerpo,:titulo,tag_ids: [])
@@ -29,13 +31,18 @@ class QuestionsController < ApplicationController
 		if @question.save
 			if @question.tags.count > 5 then
 				@question.destroy
-				redirect_to questions_path(:condicion => 0)
+				redirect_to new_question_path(:condicion => 0)
 			else
-				redirect_to questions_path(:condicion => 1)
+				redirect_to questions_path
 			end
 
 		else
 			render :new
 		end
 	end
+
+	def tinymce(config=:default, options={})
+ 		 javascript_tag { tinymce_javascript(config, options) }
+	end
+	
 end
