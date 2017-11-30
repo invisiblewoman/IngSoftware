ef099f5dda227ce222a296f98ad2e72d7ac5ccc2
MartinG <96martingalan@gmail.com>
2017-11-30 11:31:06 -0300

    Perfil begin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
 
 
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations'   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #POST "/questions/:questions_id/answers"
+  get 'users/show/:id' => "users#show", as:'perfil'
 
   resources :questions do
    resources :answers 
@@ -39,5 +40,4 @@ Rails.application.routes.draw do
   root 'questions#index'
 
 end
-
 
