83d197970a3c333b2d4f4e1898ecb9c7e719f7f6
Charo Ares <charoares96@gmail.com>
2017-12-14 12:35:22 -0300

    Mensajes y Contacto

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -3,8 +3,6 @@ class SendController < ApplicationController
 	end
 
 	def create 
-		@params= params
- 		ContactoMailer.contact_send(params).deliver
 		 flash[:notice]= "Mail Enviado"
 		 redirect_to questions_path
 	end
